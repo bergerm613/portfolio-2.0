619381b00f7a07fc80b82fd4c67edadf5e0964c8
Michelle Berger <bergerm613@gmail.com>
2023-02-01 23:55:19 -0500

    cleanup Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,72 +14,75 @@ import "./index.css";
 export const Nav = () => {
   return (
     <>
-      {/* DESKTOP */}
-      <nav className="desktop-view">
-        <CursorLink
-          href={"https://www.linkedin.com/in/michelle-berger-46aa5818b"}
-        >
-          <img src={linkedin} alt="linkedin" height="40px" />
-        </CursorLink>
-
-        <CursorLink href={"https://github.com/bergerm613"}>
-          <img src={github} alt="github" height="40px" />
-        </CursorLink>
-
-        <img
-          src={logo}
-          alt="Michelle Berger - software developer"
-          className="title"
-        />
-
-        <CursorLink href={resumePDF}>
-          <img src={resume} alt="resume" height="40px" />
-        </CursorLink>
-
-        <CursorLink
-          href={"mailto: bergerm613@gmail.com?subject=Hey, loved your website!"}
-        >
-          <img src={email} alt="email" height="40px" />
-        </CursorLink>
-      </nav>
-
-      {/* MOBILE */}
-      <nav className="mobile-view">
-        <img
-          src={logo}
-          alt="Michelle Berger - software developer"
-          className="title"
-        />
-        <div className="mobile-icons">
-          <a
-            href="https://www.linkedin.com/in/michelle-berger-46aa5818b"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={linkedin} alt="linkedin" height="40px" />
-          </a>
-
-          <a
-            href="https://github.com/bergerm613"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="github" height="40px" />
-          </a>
-
-          <a href={resumePDF} target="_blank" rel="noopener noreferrer">
-            <img src={resume} alt="resume" height="40px" />
-          </a>
-
-          <a
-            href="mailto: bergerm613@gmail.com?subject=Hey, loved your website!"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={email} alt="email" height="40px" />
-          </a>
-        </div>
-      </nav>
+      <DesktopNav />
+      <MobileNav />
     </>
   );
 };
+
+const DesktopNav = () => (
+  <nav className="desktop-view">
+    <CursorLink href={"https://www.linkedin.com/in/michelle-berger-46aa5818b"}>
+      <img src={linkedin} alt="linkedin" height="40px" />
+    </CursorLink>
+
+    <CursorLink href={"https://github.com/bergerm613"}>
+      <img src={github} alt="github" height="40px" />
+    </CursorLink>
+
+    <img
+      src={logo}
+      alt="Michelle Berger - software developer"
+      className="title"
+    />
+
+    <CursorLink href={resumePDF}>
+      <img src={resume} alt="resume" height="40px" />
+    </CursorLink>
+
+    <CursorLink
+      href={"mailto: bergerm613@gmail.com?subject=Hey, loved your website!"}
+    >
+      <img src={email} alt="email" height="40px" />
+    </CursorLink>
+  </nav>
+);
+
+const MobileNav = () => (
+  <nav className="mobile-view">
+    <img
+      src={logo}
+      alt="Michelle Berger - software developer"
+      className="title"
+    />
+    <div className="mobile-icons">
+      <a
+        href="https://www.linkedin.com/in/michelle-berger-46aa5818b"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={linkedin} alt="linkedin" height="40px" />
+      </a>
+
+      <a
+        href="https://github.com/bergerm613"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="github" height="40px" />
+      </a>
+
+      <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+        <img src={resume} alt="resume" height="40px" />
+      </a>
+
+      <a
+        href="mailto: bergerm613@gmail.com?subject=Hey, loved your website!"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={email} alt="email" height="40px" />
+      </a>
+    </div>
+  </nav>
+);
